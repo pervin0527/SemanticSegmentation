db29f098b56b88315eb2341af39877fb0f9866bc
pervin0527 <zfbtldnz@gmail.com>
2024-07-03 18:47:36 +0900

    Updated 2024.07.03

--- train.py
+++ train.py
@@ -15,8 +15,8 @@ from transformers import SegformerForSemanticSegmentation, SegformerImageProcess
 
 from data.voc import VOCDataset
 from data.bkai import BKAIDataset
-from loss import FocalLoss, dice_coefficient
-from utils.utils import Args, inference_callback, WarmupThenDecayScheduler
+from loss import FocalLoss, compute_mean_iou, compute_mean_dice_coefficient_score
+from utils.utils import Args, inference_callback, plot_metrics, WarmupThenDecayScheduler
 
 def set_seed(seed_num):
     random.seed(seed_num)
@@ -28,21 +28,10 @@ def set_seed(seed_num):
     cudnn.benchmark = False
     cudnn.deterministic = True
 
-def set_seed(seed_num):
-    random.seed(seed_num)
-    np.random.seed(seed_num)
-    
-    torch.manual_seed(seed_num)
-    torch.cuda.manual_seed(seed_num)
-    torch.cuda.manual_seed_all(seed_num)
-    cudnn.benchmark = False
-    cudnn.deterministic = True
 
-
-def valid(model, dataloader, criterion, device, num_classes, ignore_idx=0):
+def valid(model, dataloader, criterion, device, ignore_idx=0):
     model.eval()
-    losses = []
-    dice_scores = []
+    accs, losses = [], []
     with torch.no_grad():
         for idx, batch in enumerate(tqdm(dataloader, desc="Eval", leave=False)):
             pixel_values = batch["pixel_values"].to(device)
@@ -51,28 +40,26 @@ def valid(model, dataloader, criterion, device, num_classes, ignore_idx=0):
             outputs = model(pixel_values=pixel_values, labels=labels)
             loss, logits = outputs.loss, outputs.logits
             
-            upsampled_logits = F.interpolate(logits, size=labels.shape[-2:], mode="bilinear", align_corners=False)
+            upsampled_logits = F.interpolate(logits, size=labels.shape[-2:], mode="bilinear", align_corners=False) ## mode="nearest"
             predicted = upsampled_logits.argmax(dim=1)
             
-            loss = criterion(upsampled_logits, labels)
+            loss = criterion(upsampled_logits, labels) ## focal loss
             losses.append(loss.item()) 
 
-            mask = (labels != ignore_idx)
+            mask = (labels != ignore_idx) # we don't include the background class in the accuracy calculation
             pred_labels = predicted[mask].detach().cpu().numpy()
             true_labels = labels[mask].detach().cpu().numpy()
-            
-            dice = dice_coefficient(pred_labels, true_labels, num_classes)
-            dice_scores.append(dice)
+            accuracy = accuracy_score(pred_labels, true_labels)
+            accs.append(accuracy)
             
     avg_loss = sum(losses) / len(losses)
-    avg_dice = sum(dice_scores) / len(dice_scores)
-    return avg_loss, avg_dice
+    avg_acc =  sum(accs) / len(accs)
+    return avg_loss, avg_acc
 
 
-def train(model, dataloader, optimizer, criterion, device, num_classes, ignore_idx=0):
+def train(model, dataloader, optimizer, criterion, device, ignore_idx=0):
     model.train()
-    losses = []
-    dice_scores = []
+    accs, losses = [], []
     for idx, batch in enumerate(tqdm(dataloader, desc="Train", leave=False)):
         pixel_values = batch["pixel_values"].to(device)
         labels = batch["labels"].to(device)
@@ -84,23 +71,22 @@ def train(model, dataloader, optimizer, criterion, device, num_classes, ignore_i
         upsampled_logits = F.interpolate(logits, size=labels.shape[-2:], mode="bilinear", align_corners=False)
         predicted = upsampled_logits.argmax(dim=1)
 
-        loss = criterion(upsampled_logits, labels)
+        loss = criterion(upsampled_logits, labels) ## focal loss
         loss.backward()
         optimizer.step()
         losses.append(loss.item())
 
-        mask = (labels != ignore_idx)
+        mask = (labels != ignore_idx) # we don't include the background class in the accuracy calculation
         pred_labels = predicted[mask].detach().cpu().numpy()
         true_labels = labels[mask].detach().cpu().numpy()
-        
-        dice = dice_coefficient(pred_labels, true_labels, num_classes)
-        dice_scores.append(dice)
+        accuracy = accuracy_score(pred_labels, true_labels)
+        accs.append(accuracy)
 
     avg_loss = sum(losses) / len(losses)
-    avg_dice = sum(dice_scores) / len(dice_scores)
+    avg_acc =  sum(accs) / len(accs)
 
-    return avg_loss, avg_dice
-
+    return avg_loss, avg_acc
+    
 
 def main(args):
     writer = SummaryWriter(log_dir=f"{args.save_dir}/logs")
@@ -132,22 +118,22 @@ def main(args):
     criterion = FocalLoss(num_class=len(args.classes), alpha=args.focal_alpha, gamma=args.focal_gamma, reduction='mean').to(args.device)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=args.T_0, T_mult=args.T_mult, eta_min=args.min_lr)
 
-    epochs_no_improve = 0
-    max_dice_coefficient = 0.0
+    best_metric_score = 0.0
     for epoch in range(args.epochs):
         current_lr = optimizer.param_groups[0]['lr']
         print(f"\nEpoch : [{epoch+1:>03}|{args.epochs}], LR : {current_lr}")
         writer.add_scalar('Learning Rate', optimizer.param_groups[0]['lr'], epoch)
 
-        train_loss, train_dice = train(model, train_dataloader, optimizer, criterion, args.device, len(args.classes), args.semantic_loss_ignore_index)
+        train_loss, train_acc = train(model, train_dataloader, optimizer, criterion, args.device, args.semantic_loss_ignore_index)
         writer.add_scalar('Training/Loss', train_loss, epoch)
-        writer.add_scalar('Training/Dice', train_dice, epoch)
-        print(f"Train Loss : {train_loss:.4f}, Train Dice : {train_dice:.4f}")
+        writer.add_scalar('Training/Accuracy', train_acc, epoch)
+        print(f"Train Loss : {train_loss:.4f}, Train Acc : {train_acc:.4f}")
 
-        valid_loss, valid_dice = valid(model, valid_dataloader, criterion, args.device, len(args.classes), args.semantic_loss_ignore_index)
+        valid_loss, valid_acc = valid(model, valid_dataloader, criterion, args.device, args.semantic_loss_ignore_index)
         writer.add_scalar('Validation/Loss', valid_loss, epoch)
-        writer.add_scalar('Validation/Dice', valid_dice, epoch)
-        print(f"Valid Loss : {valid_loss:.4f}, Valid Dice : {valid_dice:.4f}")
+        writer.add_scalar('Validation/Accuracy', valid_acc, epoch)
+        print(f"Valid Loss : {valid_loss:.4f}, Valid Acc : {valid_acc:.4f}")
+        scheduler.step()
 
         inference_callback(args.sample_img, model, feature_extractor, args, epoch, save_dir=args.save_dir)
         if (epoch + 1) % args.metric_step == 0:
@@ -170,7 +156,6 @@ if __name__ == "__main__":
     set_seed(args.seed)
     args.num_workers = os.cpu_count()
     args.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    set_seed(args.seed)
 
     args.classes = BKAIDataset.CLASSES
     args.colormap = BKAIDataset.COLORMAP
